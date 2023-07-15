2043d95f2c9aefb3c6271228d47ecf6353c3b9ab
iwdon <87576945+iwdon@users.noreply.github.com>
2023-07-15 15:22:48 +0700

    persiapan project

--- android/app/src/main/kotlin/com/example/twitter_clone/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/twitter_clone/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.twitter_clone
+package com.dwi.twitter_clone
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+
+class AppwriteConstants {
+  static const String databaseId = '64b2362987d2b6cb17fc';
+  static const String projectId = '642933fcb0100f35d70a';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
